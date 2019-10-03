64ec2afe3e5d2d94d364ce6dca12af1b4e7a0445
Rich Caloggero <rjc@mit.edu>
2019-10-03 13:39:42 -0400

    * song-list.js now preserves links

--- blues-songs.js
+++ blues-songs.js
@@ -3,7 +3,12 @@ row.querySelectorAll("td, th").forEach((cell, cellIndex) => {
 const newCell = document.createElement(
 (rowIndex === 0 || cellIndex === 0)? "th" : "td"
 ); // createElement
+
+if (cell.querySelector("a")) {
+newCell.appendChild(cell.querySelector("a"));
+} else {
 newCell.textContent = cell.textContent;
+} // if
 cell.replaceWith(newCell);
 }); // fix cell
 }); // fix row
